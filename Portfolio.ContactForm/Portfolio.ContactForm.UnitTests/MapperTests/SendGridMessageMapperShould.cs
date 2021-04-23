745a5dcd8a5bd488f58338ee49ea6b70134bbde0
willvelida <willvelida@hotmail.co.uk>
2021-04-23 21:59:46 +1200

    Adding MapperTests test framework

---
+++ Portfolio.ContactForm/Portfolio.ContactForm.UnitTests/MapperTests/SendGridMessageMapperShould.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Portfolio.ContactForm.UnitTests.MapperTests
+{
+    public class SendGridMessageMapperShould
+    {
+        public SendGridMessageMapperShould()
+        {
+
+        }
+
+        [Fact]
+        public void ThrowArgumentNullExceptionWhenMissingRecipientEmailParameter()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void ThrowArgumentNullExceptionWhenMissingRecipientEmailName()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Fact]
+        public void SendMessageSuccessfully()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
