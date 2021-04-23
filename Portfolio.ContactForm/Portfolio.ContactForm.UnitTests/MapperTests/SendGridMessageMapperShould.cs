2308ed32b76e2a792966c9b769596a78ff4e8914
willvelida <willvelida@hotmail.co.uk>
2021-04-24 10:12:05 +1200

    Adding SendGridMessageMapper tests

--- Portfolio.ContactForm/Portfolio.ContactForm.UnitTests/MapperTests/SendGridMessageMapperShould.cs
+++ Portfolio.ContactForm/Portfolio.ContactForm.UnitTests/MapperTests/SendGridMessageMapperShould.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using FluentAssertions;
+using Microsoft.Extensions.Options;
+using Moq;
+using Newtonsoft.Json;
+using Portfolio.ContactForm.Mappers;
+using Portfolio.ContactForm.Models;
+using Portfolio.ContactForm.Models.Settings;
+using SendGrid.Helpers.Mail;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -7,39 +15,90 @@ namespace Portfolio.ContactForm.UnitTests.MapperTests
 {
     public class SendGridMessageMapperShould
     {
+        private Mock<IOptions<FunctionOptions>> _mockOptions;
+
+        private SendGridMessageMapper _func;
+
         public SendGridMessageMapperShould()
         {
-
+            _mockOptions = new Mock<IOptions<FunctionOptions>>();           
         }
 
         [Fact]
         public void ThrowArgumentNullExceptionWhenMissingRecipientEmailParameter()
         {
             // Arrange
+            FunctionOptions functionOptions = new FunctionOptions
+            {
+                RecipientName = "TestName"
+            };
+            _mockOptions.Setup(settings => settings.Value).Returns(functionOptions);
+
+            EmailMessageRequest emailMessageRequest = new EmailMessageRequest();
+            string emailMessageBody = JsonConvert.SerializeObject(emailMessageRequest);
+
+            _func = new SendGridMessageMapper(_mockOptions.Object);
 
             // Act
+            Action mapRequestToMessageAction = () => _func.MapRequestToMessage(emailMessageBody);
 
             // Assert
+            mapRequestToMessageAction.Should().Throw<ArgumentNullException>().WithMessage("Value cannot be null. (Parameter 'Missing value for parameter: RecipientEmail')");
         }
 
         [Fact]
         public void ThrowArgumentNullExceptionWhenMissingRecipientEmailName()
         {
             // Arrange
+            FunctionOptions functionOptions = new FunctionOptions
+            {
+                RecipientEmail = "test@test.com"
+            };
+            _mockOptions.Setup(settings => settings.Value).Returns(functionOptions);
+
+            EmailMessageRequest emailMessageRequest = new EmailMessageRequest();
+            string emailMessageBody = JsonConvert.SerializeObject(emailMessageRequest);
+
+            _func = new SendGridMessageMapper(_mockOptions.Object);
 
             // Act
+            Action mapRequestToMessageAction = () => _func.MapRequestToMessage(emailMessageBody);
 
             // Assert
+            mapRequestToMessageAction.Should().Throw<ArgumentNullException>().WithMessage("Value cannot be null. (Parameter 'Missing value for parameter: RecipientName')");
         }
 
         [Fact]
         public void SendMessageSuccessfully()
         {
             // Arrange
+            FunctionOptions functionOptions = new FunctionOptions
+            {
+                RecipientEmail = "test@test.com",
+                RecipientName = "TestName"
+            };
+
+            _mockOptions.Setup(settings => settings.Value).Returns(functionOptions);
+
+            EmailMessageRequest emailMessageRequest = new EmailMessageRequest
+            {
+                SenderEmail = "sender@sender.com",
+                SenderName = "Sender Name",
+                EmailSubject = "Test Email",
+                EmailBody = "This is a test email"
+            };
+            string emailMessageBody = JsonConvert.SerializeObject(emailMessageRequest);
+
+            _func = new SendGridMessageMapper(_mockOptions.Object);
 
             // Act
+            var response = _func.MapRequestToMessage(emailMessageBody);
 
             // Assert
+            Assert.Equal(new EmailAddress(emailMessageRequest.SenderEmail, emailMessageRequest.SenderName), response.From);
+            Assert.Equal(emailMessageRequest.EmailSubject, response.Subject);
+            Assert.Equal(emailMessageRequest.EmailBody, response.PlainTextContent);
+            Assert.Equal(emailMessageRequest.EmailBody, response.HtmlContent);
         }
     }
 }
