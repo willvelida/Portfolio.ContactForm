a9aa37b63a7be6a892267e95bc45eefcaa7bf89a
willvelida <willvelida@hotmail.co.uk>
2021-04-23 20:04:27 +1200

    Adding initial project and test project

---
+++ Portfolio.ContactForm/Portfolio.ContactForm.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Portfolio.ContactForm.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
